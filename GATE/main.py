c0942d013235b7efbfae222c3ae3817e124785de
João Victor <joaovictorqueimadas@gmail.com>
2025-04-30 14:31:41 -0300

    banana 04

--- GATE/main.py
+++ GATE/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from db import db
-from models import Usuario
+from models import Usuario, Evento
 from flask_login import LoginManager, login_user, login_required
 import hashlib
 
@@ -58,12 +58,30 @@ def registrar():
 
         return redirect(url_for('escolha'))# redireciona para a página de criar evento
 
+@gate.route('/gerar_qrcode1', methods=['GET', 'POST'])
+@login_required# verifica se o usuário está logado
+def gerar_qrcode1():
+    return render_template('qrcode_1.html')
 
 @gate.route('/criar_evento', methods=['GET', 'POST'])
 @login_required# verifica se o usuário está logado
 def criar_evento():
     if request.method == 'GET':
         return render_template('criar_evento.html')
+    elif request.method == 'POST':
+        nome_evento = request.form['id_evento']
+        data_evento = request.form['data_evento']
+        local_evento = request.form['local_evento']
+        horario_inicio_evento = request.form['inicio_evento']
+        horario_fim_evento = request.form['fim_evento']
+
+        novo_evento = Evento(nome_evento=nome_evento, data_evento=data_evento,
+                              local_evento=local_evento, horario_inicio_evento=horario_inicio_evento,
+                              horario_fim_evento=horario_fim_evento )
+        db.session.add(novo_evento)
+        db.session.commit()
+
+        return redirect(url_for('gerar_qrcode1'))
 
 if __name__ == "__main__":
     with gate.app_context():
--- GATE/models.py
+++ GATE/models.py
@@ -1,5 +1,6 @@
 from db import db
 from flask_login import UserMixin
+from datetime import date, time 
 
 class Usuario(UserMixin, db.Model):# herda da tabela de usuarios...
     __tablename__ = 'usuarios' # nome da tabela no banco de dados
@@ -7,6 +8,16 @@ class Usuario(UserMixin, db.Model):# herda da tabela de usuarios...
     nome = db.Column(db.String(30), unique = True) # nome do usuario
     email = db.Column(db.String(50), unique = True) # email do usuario
     senha = db.Column(db.String()) # senha do usuario
+
+
+class Evento(UserMixin, db.Model):
+    __tablename__ = 'eventos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome_evento = db.Column(db.String(100), nullable=False)
+    data_evento = db.Column(db.String(8), unique=False)
+    local_evento = db.Column(db.String(100), nullable=False)
+    horario_inicio_evento = db.Column(db.String(4), nullable=False)
+    horario_fim_evento =db.Column(db.String(4), nullable=False)
 
 #envia isso tudo para o main
 #UserMixin é uma classe do Flask-Login que fornece métodos para autenticação de usuários, como is_authenticated, is_active e is_anonymous. Isso permite que o Flask-Login saiba se o usuário está autenticado ou não.
